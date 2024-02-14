6f69336c246971428ee566f2b039f9d14b970837
Akaina1 <erinadost@gmail.com>
2024-02-14 15:29:05 -0500

    added logo

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-// App.js
 import React from 'react';
 import BackgroundVideo from './Background';
 import ScrollContainer from './ScrollContainer';
-import "./input.css"
-
+import "./input.css";
 
 const App = () => {
   return (
-    <div className="app bg-primary relative h-screen flex items-center justify-center">
+    <div className="app bg-primary relative h-screen">
       <BackgroundVideo />
       <ScrollContainer />
+      
+      {/* Logo centered at the top */}
+      <img
+        src="/IMG/InaLogo.svg"
+        alt="Ina Logo"
+        className="z-5 absolute top-0 left-1/2 transform -translate-x-1/2 w-80 h-80 object-cover"
+      />
+
       {/* Your other components/content go here */}
     </div>
   );
